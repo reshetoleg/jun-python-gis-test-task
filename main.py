889299bcefa8859d1b41ad095e27a45229bba856
developeras <gistech2015@hotmail.com>
2023-06-28 20:25:42 +0300

    Create a simple server with Flask Create a simple template upload SHP file

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 from cmath import log
 from time import sleep
 from logging import getLogger
+from flask import Flask, request, render_template, redirect, url_for
+import os
 logger = getLogger(__name__)
+app = Flask(__name__)
 
-while True:
-    logger.error("I'm doing nothing, please update me so I can do something helpful.")
-    sleep(5)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('./index.html')
+
+@app.route('/process', methods=['POST'])
+def process_file():
+    # Check if a file was uploaded
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('index'))
+
+    # Save the uploaded file
+    filename = secure_filename(file.filename)
+    file.save(filename)
+
+    # Process the uploaded file
+    result = process_shapefile(filename)
+
+    # Remove the temporary file
+    os.remove(filename)
+
+    return result
+
+def process_shapefile(filename):
+    return "OK"
+
+if __name__ == '__main__':
+    app.run()
+
+
+
