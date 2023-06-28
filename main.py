d8538d2b38ee6f5f20f872b216a3391f2f2fdb17
developeras <gistech2015@hotmail.com>
2023-06-28 21:00:29 +0300

    Update fileupload to load local roads in sample Generate generated image in base64 in lightyellow

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from time import sleep
 from logging import getLogger
 from flask import Flask, request, render_template, redirect, url_for
 import os
+import base64
+import matplotlib.pyplot as plt
+import fiona
+from colorama import init, Fore
+
 logger = getLogger(__name__)
 app = Flask(__name__)
 
@@ -13,6 +18,7 @@ def index():
 @app.route('/process', methods=['POST'])
 def process_file():
     # Check if a file was uploaded
+    '''
     if 'file' not in request.files:
         return redirect(url_for('index'))
 
@@ -21,7 +27,7 @@ def process_file():
         return redirect(url_for('index'))
 
     # Save the uploaded file
-    filename = secure_filename(file.filename)
+    filename = file.filename
     file.save(filename)
 
     # Process the uploaded file
@@ -29,11 +35,34 @@ def process_file():
 
     # Remove the temporary file
     os.remove(filename)
-
+    '''
+    result = process_shapefile('sample/roads.shp')
     return result
 
 def process_shapefile(filename):
-    return "OK"
+    init()  # Initialize colorama
+    colors = {}  # Dictionary to store unique colors for streets
+
+    with fiona.open(filename) as shp:
+        fig, ax = plt.subplots()
+
+        for street in shp:
+            color = 'lightyellow'
+            # Plot the street using the unique color
+            coords = street['geometry']['coordinates']
+            print(coords)
+            x, y = zip(*coords)
+            ax.plot(x, y, color=color)
+        # Save the figure as a PNG file
+        output_file = 'colored_streets.png'
+        plt.savefig(output_file)
+
+        # Convert the PNG file to base64 format
+        with open(output_file, 'rb') as image_file:
+            encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+        # Return the base64-encoded image
+        return encoded_image
 
 if __name__ == '__main__':
     app.run()
