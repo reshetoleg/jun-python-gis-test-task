d76ca6f20143e6e3e9318298a9e2b5dc2cc15296
developeras <gistech2015@hotmail.com>
2023-06-29 18:56:53 +0300

    Update devide shp Line strings in advance Devide Line strings in shp file in which intersect with and define its color by randomly

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import base64
 import matplotlib.pyplot as plt
 import fiona
 import random
-
+from collections import Counter
+from shapely.geometry import LineString, shape
+import geopandas as gpd
 
 logger = getLogger(__name__)
 app = Flask(__name__)
@@ -30,41 +32,64 @@ def process_shapefile(filename,type):
     styles = ['-','--','-.',':']
     roads = []
     icolor = []
-    with fiona.open(filename) as shp:
-        fig, ax = plt.subplots(figsize=(12, 12)) # Increase the figure size
-        prev = None
-        prev_color = None
-        for street in shp:
-     
-            roads, road_index = getRoadIndex(roads,street)
-            if(road_index == 0):
-                color = colors[random.randint(0, 5)]
-                coords = street['geometry']['coordinates']
-                roads.append([coords[0],coords[-1]])
-                icolor.append(color)
-            else:
-                color = icolor[road_index]
-          
-            style = styles[random.randint(0, 3)]
-            # Plot the street using the unique color
-            coords = street['geometry']['coordinates']
-            x, y = zip(*coords)
-            if int(type) == OUTPUT_TYPE_COLOURIZED:
-                ax.plot(x, y, color=color)
-            else:
-                ax.plot(x, y, linestyle=style,color=color)
-            prev = street
-            prev_color = color
-        # Save the figure as a PNG file
-        output_file = 'colored_streets.png'
-        plt.savefig(output_file)
+    data = gpd.read_file(filename)
+    street_lines = []
+    for row in data.iterrows():
+        geometry = row[1].geometry
+        if isinstance(geometry, LineString):
+            street_lines.append(geometry)
+    plotted_lines = colorize_streets(street_lines)
 
-        # Convert the PNG file to base64 format
-        with open(output_file, 'rb') as image_file:
-            encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+    # Generate the image with colored streets
+    fig, ax = plt.subplots(figsize=(12, 12)) # Increase the figure size
+    for x, y, color in plotted_lines:
+        style = styles[random.randint(0, 3)]
+        if int(type) == OUTPUT_TYPE_COLOURIZED:
+            ax.plot(x, y, color=color)
+        else:
+            ax.plot(x, y, linestyle=style,color=color)
 
-        # Return the base64-encoded image
-        return encoded_image
+    # Save the figure as a PNG file
+    output_file = 'colored_streets.png'
+    plt.savefig(output_file)
+
+    # Convert the PNG file to base64 format
+    with open(output_file, 'rb') as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+    # Return the base64-encoded image
+    return encoded_image
+
+def colorize_streets(lines):
+    streets = {}
+    colored_lines = []
+
+    for line in lines:
+        merged = False
+        # Check if the current line intersects with any existing street
+        for street_id, street in streets.items():
+            if street.intersects(line):
+                streets[street_id] = street.union(line)
+                colored_lines.append((line, street_id))
+                merged = True
+                break
+        
+        # If the line doesn't intersect with any existing street, create a new street
+        if not merged:
+            street_id = len(streets) + 1
+            streets[street_id] = line
+            colored_lines.append((line, street_id))
+
+    # Assign random colors to each street
+    colors = ['#%06x' % random.randint(0, 0xFFFFFF) for _ in range(len(streets))]
+
+    # Convert lines to (x, y) coordinates for plotting
+    plotted_lines = []
+    for line, street_id in colored_lines:
+        x, y = line.xy
+        plotted_lines.append((x, y, colors[street_id - 1]))
+
+    return plotted_lines
 
 def is_connected(coords1, coords2):
     # Implement your logic to check if two lines are connected
@@ -95,12 +120,13 @@ def getRoadIndex(roads,street):
         min_heading = 0
         index = 0
         for i,heading in connected:
-            print(i,heading)
             if heading>min_heading:
                 min_heading = heading
                 index = i
-       
-        roads[index] =  list(set([c_start,c_end,roads[index][0], roads[index][-1]]))
+        counter = Counter([c_start,c_end,roads[index][0], roads[index][1]])
+        merged  = [x for x in [c_start,c_end,roads[index][0], roads[index][1]] if counter[x] == 1]
+        if len(merged)>0:
+            roads[index] = merged
         return roads,index
 
 def getHeading(start_point, end_point):
